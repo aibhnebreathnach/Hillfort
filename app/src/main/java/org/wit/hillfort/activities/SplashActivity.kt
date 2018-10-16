61853a3e381f14029d43ffeea3f62891b031b95a
Aibhne Breathnach <aibhnebreathnach@gmail.com>
2018-10-16 19:32:46 +0100

    Added splash screen from tutorial : https://devofandroid.blogspot.com/2018/03/splash-screen-kotlin-android-studio.html

---
+++ app/src/main/java/org/wit/hillfort/activities/SplashActivity.kt
@@ -0,0 +1,28 @@
+package org.wit.hillfort.activities
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.Window
+import android.view.WindowManager
+import org.wit.hillfort.R
+
+class SplashActivity : AppCompatActivity() {
+
+  val wait:Long = 3000 // 3 sec wait
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    window.requestFeature(Window.FEATURE_NO_TITLE) // hiding title bar of this activity
+    window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN) // making this activity full screen
+    setContentView(R.layout.activity_splash)
+
+    // Start this activity and wait to start Main Activity
+    Handler().postDelayed({
+      startActivity(Intent(this, HillfortListActivity::class.java)) // start main activity
+      finish() // finish this activity
+    }, wait)
+
+  }
+}
